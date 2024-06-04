1129c348f389b2b0536e5e71bd6864491deac90e
ukm3423 <ukm.office3423@gmail.com>
2024-06-04 18:03:31 +0530

    master-service

---
+++ src/main/java/com/masterservice/MasterserviceApplication.java
@@ -0,0 +1,22 @@
+package com.masterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * * ===========================================================================
+ * * ======================== Module : Master-Service ==========================
+ * * ======================== Created By : Umesh Kumar =========================
+ * * ======================== Created On : 04-06-2024 ==========================
+ * * ===========================================================================
+ * * | Code Status : On
+ */
+
+@SpringBootApplication
+public class MasterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterserviceApplication.class, args);
+	}
+
+}
